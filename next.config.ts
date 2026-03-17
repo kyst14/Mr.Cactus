a57faf3d44f3bef0dc56495eea6059e9c22db93c
kyst14 <284.1izm@gmail.com>
2026-03-17 19:42:24 +0200

    update packages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: process.env.ALLOWED_ORIGINS?.split(",").map((origin) => origin.trim()) || [],
+  reactStrictMode: true,
 };
 
 export default nextConfig;
