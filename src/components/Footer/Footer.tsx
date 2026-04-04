39873761efb0dde2e9249c058ea6cda26806a328
kyst14 <284.1izm@gmail.com>
2026-04-04 13:45:44 +0300

    add more social in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,20 +1,40 @@
-import { SocialIcon } from './SocialIcon'
-import { FooterLink } from './FooterLink'
 import { PAGES } from '@/config/pages.config'
+import { FooterLink } from './FooterLink'
+import { SocialIcon } from './SocialIcon'
 // import ThemeSwitcher from '../Theme/ThemeSwitcher'
 
 export const Footer = () => {
 	return (
 		<footer className="relative flex justify-center items-center text-text text-2xl font-bold p-0 sm:p-2">
 			<div className="relative flex flex-col justify-center items-center gap-5 border-2 border-text w-full h-full p-5 before:absolute before:bg-primary/60 before:top-0 before:left-0 sm:before:bg-primary/40 sm:before:top-1.5 sm:before:left-1.5 before:w-full before:h-full before:-z-10 sm:hover:before:-translate-x-1.5 sm:hover:before:-translate-y-1.5 before:duration-300 hover:before:bg-primary/60">
-				<h1 className="font-heading font-bold text-3xl tracking-wider">MR.CACTUS</h1>
+				<h1 className="font-heading font-bold text-3xl tracking-wider">
+					MR.CACTUS
+				</h1>
 
-				<div className="flex gap-5"> {/* Socials */}
-					<SocialIcon href="https://instagram.com/mr.cactus360" icon='instagram' />
-					<SocialIcon href="https://github.com/kyst14/mr.cactus" icon='github' />
+				<div className="flex gap-5">
+					{' '}
+					{/* Socials */}
+					<SocialIcon
+						href="https://instagram.com/mr.cactus360"
+						icon="instagram"
+					/>
+					<SocialIcon
+						href="https://www.facebook.com/share/1Dnbo1Q87h/?mibextid=wwXIfr"
+						icon="facebook"
+					/>
+					<SocialIcon
+						href="viber://chat?number=+380671629405"
+						icon="viber"
+					/>
+					<SocialIcon
+						href="https://github.com/kyst14/mr.cactus"
+						icon="github"
+					/>
 				</div>
 
-				<div> {/* Links */}
+				<div>
+					{' '}
+					{/* Links */}
 					<ul className="flex gap-5">
 						<FooterLink href={PAGES.HOME}>Головна</FooterLink>
 						<FooterLink href={PAGES.CATALOG}>Каталог</FooterLink>
@@ -26,7 +46,9 @@ export const Footer = () => {
 					<ThemeSwitcher />
 				</div> */}
 
-				<div> {/* Copyright */}
+				<div>
+					{' '}
+					{/* Copyright */}
 					<p className="text-center text-sm">
 						©2026 Mr.Cactus. All rights reserved.
 					</p>
--- src/components/Footer/SocialIcon.tsx
+++ src/components/Footer/SocialIcon.tsx
@@ -1,18 +1,37 @@
-import { faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Link from 'next/link';
+import { faGithub, faInstagram, faFacebook, faViber } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
 
 const icons = {
 	github: faGithub,
-	instagram: faInstagram
-};
+	instagram: faInstagram,
+	facebook: faFacebook,
+	viber: faViber,
+}
 
-type IconName = keyof typeof icons;
+type IconName = keyof typeof icons
 
-export const SocialIcon = ({ href, icon }: { href: string, icon: IconName }) => {
+export const SocialIcon = ({
+	href,
+	icon
+}: {
+	href: string
+	icon: IconName
+}) => {
 	return (
-		<Link href={href} target="_blank" rel="noopener noreferrer" aria-label={icon} className="flex items-center justify-center text-text/70 hover:text-text">
-			<FontAwesomeIcon icon={icons[icon]} className="fill-current transition-[fill] duration-300" width={40} height={40} />
+		<Link
+			href={href}
+			target="_blank"
+			rel="noopener noreferrer"
+			aria-label={icon}
+			className="flex items-center justify-center text-text/70 hover:text-text"
+		>
+			<FontAwesomeIcon
+				icon={icons[icon]}
+				className="fill-current transition-[fill] duration-300"
+				width={40}
+				height={40}
+			/>
 		</Link>
 	)
 }
