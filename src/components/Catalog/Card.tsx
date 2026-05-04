35a7c9c709871e0ef58b375a7751da06245cf491
kyst14 <284.1izm@gmail.com>
2026-05-04 18:24:06 +0300

    fix bug

--- src/components/Catalog/Card.tsx
+++ src/components/Catalog/Card.tsx
@@ -11,7 +11,7 @@ export const Card = ({ data }: { data: CatalogType }) => {
 			<div className="flex flex-col items-center justify-center gap-4 bg-bg shadow-lg p-5 rounded hover:scale-105 transition-transform duration-300 ease-in-out">
 				<div className="flex items-center justify-center">
 					<Image
-						src={data.image_urls[0] || '/placeholder.svg'}
+						src={data.mediaUrls[0] || '/placeholder.svg'}
 						alt={data.name}
 						width={200}
 						height={200}
@@ -21,7 +21,7 @@ export const Card = ({ data }: { data: CatalogType }) => {
 				<div className="flex flex-col items-center justify-center">
 					<h1 className="text-2xl font-bold">{data.name}</h1>
 					<p className="text-lg font-light">{data.description}</p>
-					<p className="text-lg font-bold">₴{data.price_min} - ₴{data.price_max}</p>
+					<p className="text-lg font-bold">₴{data.priceMin} - ₴{data.priceMax}</p>
 				</div>
 			</div>
 		</Link>
